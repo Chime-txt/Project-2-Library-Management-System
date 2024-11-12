ea33f2415b3c85616ff68bdd4887629d7ecec41d
tdn3411 <tdn3411@mavs.uta.edu>
2024-11-12 12:20:39 -0600

    Fixed CREATE syntax errors
    Moved Table name after IF NOT EXISTS

--- sql_tables.sql
+++ sql_tables.sql
@@ -4,7 +4,7 @@
 
 -- PUBLISHER: Publisher_Name, Phone, Address
 -- PUBLISHER Table Creator: Chime Nguyen
-CREATE TABLE PUBLISHER IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS PUBLISHER (
     -- Attributes
     Publisher_Name VARCHAR(27) NOT NULL, -- The number 27 came from the longest name from the publisher in the DB
     Phone CHAR(12) NOT NULL, -- The number 12 came from the max length of the phone number (excluding extensions)
@@ -18,7 +18,7 @@ CREATE TABLE PUBLISHER IF NOT EXISTS (
 
 -- LIBRARY_BRANCH: Branch_Id, Branch_Name, Branch_Address
 -- LIBRARY_BRANCH Table Creator: Chime Nguyen
-CREATE TABLE LIBRARY_BRANCH IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS LIBRARY_BRANCH (
     -- Attributes
     Branch_Id INT NOT NULL,
     Branch_Name  NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE LIBRARY_BRANCH IF NOT EXISTS (
 
 -- BOOK_LOANS: Book_Id, Branch_Id, Card_No, Date_Out, Due_Date, Returned_date
 -- BOOK_LOANS Table Creator: Chime Nguyen
-CREATE TABLE BOOK_LOANS IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS BOOK_LOANS (
     -- Attributes
     Book_Id INT NOT NULL,
     Branch_Id INT NOT NULL,
@@ -64,7 +64,7 @@ CREATE TABLE BOOK_LOANS IF NOT EXISTS (
 
 -- BOOK_COPIES: Book_Id, Branch_Id, No_Of_Copies
 -- BOOK_COPIES Table Creator: Trung Nguyen
-CREATE TABLE BOOK_COPIES IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS BOOK_COPIES (
 	-- Attributes
 	Book_Id INT NOT NULL,
 	Branch_Id INT NOT NULL,
@@ -80,7 +80,7 @@ CREATE TABLE BOOK_COPIES IF NOT EXISTS (
 
 -- BOOK_AUTHORS: Book_Id, Author_Name
 -- BOOK_AUTHORS Table Creator: Trung Nguyen
-CREATE TABLE BOOK_AUTHORS IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS BOOK_AUTHORS (
 	Book_Id INT NOT NULL,
 	Author_Name VARCHAR(30) -- arbitrary 30 value
 
