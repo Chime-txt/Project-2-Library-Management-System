7f1998ac0dfde074c49b86bbe892c0e15afc25c8
Chime-txt <122952454+Chime-txt@users.noreply.github.com>
2024-11-23 11:13:30 -0600

    Create Tables In Python Script
    - Imported the tables into the Python Script
    - Next, I'll test whether it imports properly, and make any additional changes in case I need to revert back to an older commit

--- TkinterGUI.py
+++ TkinterGUI.py
@@ -11,62 +11,64 @@ import sqlite3
 # Create tkinter window
 root = Tk()
 
-root.title('Address Book')
+root.title('Library Management System')
 root.geometry("400x400")
 
 
 
-# Connect to the Library Management System Database and create the tables, if they are not created
+# Connect to or create a Library Management System Database and create the tables, if they are not created
 conn = sqlite3.connect('library_management_system.db')
-add_book_c = conn.cursor()
+
+# Create a corsor
+create_tables = conn.cursor()
 
 
 
 # Add Information To The Database
-# def submit():
-# 	submit_conn = sqlite3.connect('library_management_system.db')
-# 	submit_cur = submit_conn.cursor()
+    # def submit():
+    # 	submit_conn = sqlite3.connect('library_management_system.db')
+    # 	submit_cur = submit_conn.cursor()
 
-# 	submit_cur.execute("INSERT INTO ADDRESSES VALUES (:fname, :lname, :street, :city, :state, :zcode)",
-# 		{
-# 			'fname': f_name.get(),
-# 			'lname': l_name.get(),
-# 			'street': street.get(),
-# 			'city': city.get(),
-# 			'state': state.get(),
-# 			'zcode': zcode.get(),
-# 		})
+    # 	submit_cur.execute("INSERT INTO ADDRESSES VALUES (:fname, :lname, :street, :city, :state, :zcode)",
+    # 		{
+    # 			'fname': f_name.get(),
+    # 			'lname': l_name.get(),
+    # 			'street': street.get(),
+    # 			'city': city.get(),
+    # 			'state': state.get(),
+    # 			'zcode': zcode.get(),
+    # 		})
 
-# 	submit_conn.commit()
-# 	submit_conn.close()
+    # 	submit_conn.commit()
+    # 	submit_conn.close()
 
 
 
 # Given city and state return first and last name
-# def input_query():
+    # def input_query():
 
-# 	iq_conn = sqlite3.connect('library_management_system.db')
-# 	iq_cur = iq_conn.cursor()
+    # 	iq_conn = sqlite3.connect('library_management_system.db')
+    # 	iq_cur = iq_conn.cursor()
 
-# 	iq_cur.execute("SELECT FIRST_NAME, LAST_NAME FROM ADDRESSES WHERE CITY = ? AND STATE = ?",
-# 					(city.get(), state.get(),)) 
+    # 	iq_cur.execute("SELECT FIRST_NAME, LAST_NAME FROM ADDRESSES WHERE CITY = ? AND STATE = ?",
+    # 					(city.get(), state.get(),)) 
 
 
-# 	records = iq_cur.fetchall()
+    # 	records = iq_cur.fetchall()
 
-# 	print(records)
+    # 	print(records)
 
-# 	print_records = ''
+    # 	print_records = ''
 
-# 	for record in records:
-# 		print_records += str(record[0] + " " + record[1] + "\n")
+    # 	for record in records:
+    # 		print_records += str(record[0] + " " + record[1] + "\n")
 
-# 	iq_label = Label(root, text = print_records)
-# 	iq_label.grid(row = 9, column = 0, columnspan = 2)
+    # 	iq_label = Label(root, text = print_records)
+    # 	iq_label.grid(row = 9, column = 0, columnspan = 2)
 
 
 # Add Tables Into DB Via Python
-# add_book_c.execute('''
+# create_tables.execute('''
 #	 CREATE TABLE ADDRESSES(FIRST_NAME TEXT,
 #	 	LAST_NAME TEXT,
 #	 	ADDRESS TEXT,
@@ -75,6 +77,76 @@ add_book_c = conn.cursor()
 #	 	ZIPCODE INT
 #	 )
 # ''')
+
+# Create Library Management System tables
+create_tables.execute('''
+    CREATE TABLE IF NOT EXISTS PUBLISHER (
+        -- Test Text
+        Publisher_Name VARCHAR(27) NOT NULL,
+        Phone CHAR(12) NOT NULL,
+        Address VARCHAR(120) NOT NULL,
+
+        PRIMARY KEY (Publisher_Name)
+    )
+
+    CREATE TABLE IF NOT EXISTS LIBRARY_BRANCH (
+        Branch_Id INTEGER PRIMARY KEY NOT NULL,
+        Branch_Name VARCHAR(11) NOT NULL,
+        Branch_Address VARCHAR(120) NOT NULL
+    )
+
+    CREATE TABLE IF NOT EXISTS BORROWER (
+        Card_No INTEGER PRIMARY KEY NOT NULL,
+        Name VARCHAR(64) NOT NULL,
+        Address VARCHAR(120) NOT NULL,
+        Phone CHAR(12) NOT NULL,
+
+        UNIQUE (Phone)
+    )
+
+    CREATE TABLE IF NOT EXISTS BOOK (
+        Book_Id INTEGER PRIMARY KEY NOT NULL,
+        Title VARCHAR (128) NOT NULL,
+        Publisher_name VARCHAR(64) NOT NULL,
+
+        FOREIGN KEY (Publisher_name) REFERENCES PUBLISHER (Publisher_Name)
+    )
+
+    CREATE TABLE IF NOT EXISTS BOOK_LOANS (
+        Book_Id INTEGER NOT NULL,
+        Branch_Id INTEGER NOT NULL,
+        Card_No INTEGER NOT NULL,
+        Date_Out TEXT NOT NULL,
+        Due_Date TEXT NOT NULL,
+        Returned_date TEXT NOT NULL,
+
+        PRIMARY KEY (Book_Id, Branch_Id, Card_No),
+
+        FOREIGN KEY (Book_Id) REFERENCES BOOK (Book_Id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (Branch_Id) REFERENCES LIBRARY_BRANCH (Branch_Id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (Card_No) REFERENCES BORROWER (Card_No) ON UPDATE CASCADE ON DELETE CASCADE
+    )
+
+    CREATE TABLE IF NOT EXISTS BOOK_COPIES (
+        Book_Id INTEGER NOT NULL,
+        Branch_Id INTEGER NOT NULL,
+        No_Of_Copies INT NOT NULL,
+        
+        PRIMARY KEY (Book_Id, Branch_Id),
+
+        FOREIGN KEY (Book_Id) REFERENCES BOOK (Book_Id) ON UPDATE CASCADE ON DELETE CASCADE, -- Match Book_Id from Book
+        FOREIGN KEY (Branch_Id) REFERENCES LIBRARY_BRANCH (Branch_Id) ON UPDATE CASCADE ON DELETE CASCADE -- Match Branch-Id from Library_Branch
+    )
+
+    CREATE TABLE IF NOT EXISTS BOOK_AUTHORS (
+        Book_Id INTEGER NOT NULL,
+        Author_Name VARCHAR(30),
+
+        PRIMARY KEY (Book_Id, Author_Name),
+
+        FOREIGN KEY (Book_Id) REFERENCES BOOK(Book_Id) ON UPDATE CASCADE ON DELETE CASCADE
+    )
+''')
 
 
 
@@ -132,7 +204,41 @@ add_book_c = conn.cursor()
 # input_query_btn = Button(root, text = "Show Records", command = input_query)
 # input_query_btn.grid(row = 7, column = 0, columnspan = 2, pady = 10, padx = 10, ipadx = 100)
 
+# Project 2 Part 2 Queries
+part_2_query_1 = Button(root, text = "Project 2, Query 1", command = )
+part_2_query_1.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
 
+part_2_query_2 = Button(root, text = "Project 2, Query 2", command = )
+part_2_query_2.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+part_2_query_3 = Button(root, text = "Project 2, Query 3", command = )
+part_2_query_3.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+part_2_query_4a = Button(root, text = "Project 2, Query 4a", command = )
+part_2_query_4a.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+part_2_query_4b = Button(root, text = "Project 2, Query 4b", command = )
+part_2_query_4b.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+part_2_query_5 = Button(root, text = "Project 2, Query 5", command = )
+part_2_query_5.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+part_2_query_6 = Button(root, text = "Project 2, Query 6", command = )
+part_2_query_6.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+part_2_query_7 = Button(root, text = "Project 2, Query 7", command = )
+part_2_query_7.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+part_2_query_8 = Button(root, text = "Project 2, Query 8", command = )
+part_2_query_8.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+part_2_query_9 = Button(root, text = "Project 2, Query 9", command = )
+part_2_query_9.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+part_2_query_10 = Button(root, text = "Project 2, Query 10", command = )
+part_2_query_10.grid(row = , column = , columnspan = , pady = , padx = , ipadx = )
+
+# Project 2 Part 3 Queries
 
 # Run Main Loop
 root.mainloop()
